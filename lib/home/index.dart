2273b08ab1dbfa36920ae5827b977eb0141161b9
Chau Lai Gia Thanh <chaulaigiathanh@gmail.com>
2024-03-04 21:52:45 +0700

    homepage

---
+++ lib/home/index.dart
@@ -0,0 +1,56 @@
+import "package:flutter/material.dart";
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Home"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              // mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                    child: SizedBox(
+                  width: 200.0,
+                  height: 150.0,
+                  child: Image.network(
+                      "https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg"),
+                )),
+                const Expanded(
+                  child: SizedBox(
+                    height: 150.0,
+                    child: Padding(
+                      padding: EdgeInsets.all(5.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text("Sương sáo"),
+                          Text("Món ăn giải nhiệt ngày hè."),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
